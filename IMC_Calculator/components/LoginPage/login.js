f64b8701168aa2253660da16b8c7fbf91ecdb9ef
jciterceros <jciterceros@yahoo.com>
2022-04-15 21:40:38 -0300

    add Switch

--- IMC_Calculator/components/LoginPage/login.js
+++ IMC_Calculator/components/LoginPage/login.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     ImageBackground,
     StyleSheet,
     Text,
     View,
     TextInput,
+    Button,
+    Pressable,
+    Alert,
+    Switch,
 } from "react-native";
 
 import image from "../../components/imgs/Avatar_Login.png";
 //const image = { uri: "https://reactjs.org/logo-og.png" };
 
 export function Login() {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
     return (
         <View style={styles.container}>
             <View style={styles.avatarContainer}>
@@ -36,6 +43,21 @@ export function Login() {
                     placeholder="Digite aqui sua senha"
                 />
             </View>
+            <View style={styles.footer}>
+                <Switch
+                    trackColor={{ false: "#767577", true: "green" }}
+                    thumbColor={isEnabled ? "green" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                />
+                <Pressable
+                    style={styles.button}
+                    onPress={() => Alert.alert("Login efetuado com sucesso!")}
+                >
+                    <Text style={styles.buttonText}>{"Entrar"}</Text>
+                </Pressable>
+            </View>
         </View>
     );
 }
@@ -43,7 +65,7 @@ export function Login() {
 const styles = StyleSheet.create({
     container: {
         //flex: 1,
-        height: "94%",
+        height: "98%",
         width: "98%",
         marginTop: "10%",
         //backgroundColor: "green",
@@ -53,15 +75,17 @@ const styles = StyleSheet.create({
         //borderWidth: 1,
     },
     avatarContainer: {
-        marginTop: "40%",
-        height: 150,
-        width: 150,
+        marginTop: "20%",
+        height: 200,
+        width: 200,
+        borderRadius: 40,
         //backgroundColor: "green",
         //alignItems: "center",
         //justifyContent: "center",
-        //borderColor: "#fff",
-        //borderWidth: 1,
-        marginBottom: 50,
+        borderColor: "#fff",
+        borderWidth: 1,
+        padding: 20,
+        marginBottom: 30,
     },
     image: {
         width: "100%",
@@ -101,5 +125,32 @@ const styles = StyleSheet.create({
         marginBottom: 5,
         display: "flex",
         flexDirection: "row",
+        marginBottom: 10,
+    },
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: "black",
+        marginLeft: "10%",
+    },
+    buttonText: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: "bold",
+        letterSpacing: 0.25,
+        color: "white",
+    },
+    footer: {
+        width: "100%",
+        height: "10%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        //backgroundColor: "red",
     },
 });
